3b56e024decc03f3fd30a92d76d8f3c1a6b35363
Riyadevvarshney11 <riyadevvarshney11@gmail.com>
2021-07-07 16:42:53 +0530

    .

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ module.exports = connectDB;*/
 mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        useFindAndModify: false,
+        // useFindAndModify: false,
     })
     .then(() => {
         console.log("database connected");
